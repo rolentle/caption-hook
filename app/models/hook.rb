ea6e685b3409c1cdb9ebf2ac5b6101be8a755645
Rolen Le <rolentle@gmail.com>
2014-02-26 09:18:09 -0700

    wip faraday

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -1,20 +1,42 @@
+require 'uri'
 class Hook < ActiveRecord::Base
 
   def post_message(message)
-    Faraday.post url, hook_params(message)
+    raise StandardError unless url_path
+    connection.post do |req|
+      req.url url_path
+      hook_params(message).each do |k,v|
+        req.params[k.to_s] = v
+      end
+    end
   end
 
   def hook_params(message)
     valid_params.each_with_object({}) do |param, hash|
-      hash[param] = message.send(param.to_sym)
+      hash[param] = message.send(param)
     end
   end
 
   def valid_params
-    params = { "app" => app, "user" => user,
-               "post_url" => post_url, "head" => head,
-               "head_long" => head_long, "git_log" => git_log}
+    params = { app: app, user: user, post_url: post_url,
+               head: head, head_long:  head_long, git_log: git_log}
     params.keys.select { |param| params[param] == true }
   end
 
+  def connection
+    Faraday.new(:url => url_domain) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def url_domain
+    uri = URI.parse(url)
+    "#{uri.scheme}://#{uri.host}"
+  end
+
+  def url_path
+    URI.parse(url).path
+  end
 end
--- test/models/hook_test.rb
+++ test/models/hook_test.rb
@@ -1,10 +1,25 @@
 require 'test_helper'
 
 class HookTest < ActiveSupport::TestCase
-  test "it knows its valid params" do
-    hook = Hook.create(app: true, user: true)
-    params = hook.valid_params
 
-    assert params.include? "app"
+  test "it knows the domain and path" do
+    hook = Hook.create(url: "http://hook-ends.herokuapp.com/hooks", app: true, user: true)
+    assert_equal "http://hook-ends.herokuapp.com", hook.url_domain
+    assert_equal "/hooks", hook.url_path
   end
+
+
+  test "it knows its valid params" do
+    hook = Hook.create(url: "http://hook-ends.herokuapp.com/hooks", app: true, user: true)
+    params = hook.valid_params
+    assert params.include? :app
+  end
+
+  test "hook params return hash of params" do
+    message = Message.create(app: "rolen-example", user: "rolentle@gmail.com", url: "http://rolen-example.herokuapp.com", head: "73101b7", head_long: "73101b7757dea3079293515ee68b22df247213c6", git_log: "* Rolen Le: edits")
+    hook = Hook.create(url: "http://hook-ends.herokuapp.com/hooks", app: true, user: true)
+    params = hook.hook_params(message)
+    assert_equal params, { app: "rolen-example", user: "rolentle@gmail.com" }
+  end
+
 end
